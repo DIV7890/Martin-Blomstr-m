47782c4c398269db1ca5a5de86557ca57b5817a7
LUkas Vennman <lukven060503@student.jenseneducation.se>
2023-03-29 09:50:40 +0200

    Neigour

--- Dungeon2.py
+++ Dungeon2.py
@@ -337,64 +337,93 @@ class Enemy(Entity):
             if r == 1:
                 if coins_on_screen < 10:
                     if coins_on_screen == 0:
-                        CoinXposs1 = self.x
-                        CoinYposs1 = self.y
-                        Coin1 = Object(CoinXposs1 + 5, CoinYposs1 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs1 == 0 and CoinYposs1 == 0:
+                            CoinXposs1 = self.x
+                            CoinYposs1 = self.y
+                            Coin1 = Object(CoinXposs1 + 5, CoinYposs1 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 1 fixade nya kordinater och den har skapats på kordinat " + str(
+                                self.x) + " " + str(self.y))
 
                     elif coins_on_screen == 1:
-                        CoinXposs2 = self.x
-                        CoinYposs2 = self.y
-                        Coin2 = Object(CoinXposs2 + 5, CoinYposs2 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs2 == 0 and CoinYposs2 == 0:
+                            CoinXposs2 = self.x
+                            CoinYposs2 = self.y
+                            Coin2 = Object(CoinXposs2 + 5, CoinYposs2 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 2 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 2:
-                        CoinXposs3 = self.x
-                        CoinYposs3 = self.y
-                        Coin3 = Object(CoinXposs3 + 5, CoinYposs3 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs3 == 0 and CoinYposs3 == 0:
+                            CoinXposs3 = self.x
+                            CoinYposs3 = self.y
+                            Coin3 = Object(CoinXposs3 + 5, CoinYposs3 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 3 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 3:
-                        CoinXposs4 = self.x
-                        CoinYposs4 = self.y
-                        Coin4 = Object(CoinXposs4 + 5, CoinYposs4 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs4 == 0 and CoinYposs4 == 0:
+                            CoinXposs4 = self.x
+                            CoinYposs4 = self.y
+                            Coin4 = Object(CoinXposs4 + 5, CoinYposs4 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 4 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 4:
-                        CoinXposs5 = self.x
-                        CoinYposs5 = self.y
-                        Coin5 = Object(CoinXposs5 + 5, CoinYposs5 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs5 == 0 and CoinYposs5 == 0:
+                            CoinXposs5 = self.x
+                            CoinYposs5 = self.y
+                            Coin5 = Object(CoinXposs5 + 5, CoinYposs5 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 5 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 5:
-                        CoinXposs6 = self.x
-                        CoinYposs6 = self.y
-                        Coin6 = Object(CoinXposs6 + 5, CoinYposs6 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs6 == 0 and CoinYposs6 == 0:
+                            CoinXposs6 = self.x
+                            CoinYposs6 = self.y
+                            Coin6 = Object(CoinXposs6 + 5, CoinYposs6 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 6 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 6:
-                        CoinXposs7 = self.x
-                        CoinYposs7 = self.y
-                        Coin7 = Object(CoinXposs7 + 5, CoinYposs7 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs7 == 0 and CoinYposs7 == 0:
+                            CoinXposs7 = self.x
+                            CoinYposs7 = self.y
+                            Coin7 = Object(CoinXposs7 + 5, CoinYposs7 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 7 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 7:
-                        CoinXposs8 = self.x
-                        CoinYposs8 = self.y
-                        Coin8 = Object(CoinXposs8 + 5, CoinYposs8 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs8 == 0 and CoinYposs8 == 0:
+                            CoinXposs8 = self.x
+                            CoinYposs8 = self.y
+                            Coin8 = Object(CoinXposs8 + 5, CoinYposs8 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 8 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 8:
-                        CoinXposs9 = self.x
-                        CoinYposs9 = self.y
-                        Coin9 = Object(CoinXposs9 + 5, CoinYposs9 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs9 == 0 and CoinYposs9 == 0:
+                            CoinXposs9 = self.x
+                            CoinYposs9 = self.y
+                            Coin9 = Object(CoinXposs9 + 5, CoinYposs9 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 9 fixade nya kordinater " + str(self.x) + " " + str(self.y))
+
 
                     elif coins_on_screen == 9:
-                        CoinXposs10 = self.x
-                        CoinYposs10 = self.y
-                        Coin10 = Object(CoinXposs10 + 5, CoinYposs10 + 5, 25, 25, pygame.image.load("Coin.png"))
-                        coins_on_screen += 1
+                        if CoinXposs10 == 0 and CoinYposs10 == 0:
+                            CoinXposs10 = self.x
+                            CoinYposs10 = self.y
+                            Coin10 = Object(CoinXposs10 + 5, CoinYposs10 + 5, 25, 25, pygame.image.load("Coin.png"))
+                            coins_on_screen += 1
+                            print("Coin 10 fixade nya kordinater " + str(self.x) + " " + str(self.y))
 
     def destroy(self):
         objects.remove(self)
@@ -885,25 +914,55 @@ while True:
 
             if coins_on_screen != 0:
                 if CoinXposs1 != 0 and CoinYposs1 != 0 and Coin1 in objects:
+                    CoinXposs1 = 0
+                    CoinYposs1 = 0
                     objects.remove(Coin1)
+                    coins_on_screen -= 1
                 if CoinXposs2 != 0 and CoinYposs2 != 0 and Coin2 in objects:
+                    CoinXposs2 = 0
+                    CoinYposs2 = 0
                     objects.remove(Coin2)
+                    coins_on_screen -= 1
                 if CoinXposs3 != 0 and CoinYposs3 != 0 and Coin3 in objects:
+                    CoinXposs3 = 0
+                    CoinYposs3 = 0
                     objects.remove(Coin3)
+                    coins_on_screen -= 1
                 if CoinXposs4 != 0 and CoinYposs4 != 0 and Coin4 in objects:
+                    CoinXposs4 = 0
+                    CoinYposs4 = 0
                     objects.remove(Coin4)
+                    coins_on_screen -= 1
                 if CoinXposs5 != 0 and CoinYposs5 != 0 and Coin5 in objects:
+                    CoinXposs5 = 0
+                    CoinYposs5 = 0
                     objects.remove(Coin5)
+                    coins_on_screen -= 1
                 if CoinXposs6 != 0 and CoinYposs6 != 0 and Coin6 in objects:
+                    CoinXposs6 = 0
+                    CoinYposs6 = 0
                     objects.remove(Coin6)
+                    coins_on_screen -= 1
                 if CoinXposs7 != 0 and CoinYposs7 != 0 and Coin7 in objects:
+                    CoinXposs7 = 0
+                    CoinYposs7 = 0
                     objects.remove(Coin7)
+                    coins_on_screen -= 1
                 if CoinXposs8 != 0 and CoinYposs8 != 0 and Coin8 in objects:
+                    CoinXposs8 = 0
+                    CoinYposs8 = 0
                     objects.remove(Coin8)
+                    coins_on_screen -= 1
                 if CoinXposs9 != 0 and CoinYposs9 != 0 and Coin9 in objects:
+                    CoinXposs9 = 0
+                    CoinYposs9 = 0
                     objects.remove(Coin9)
+                    coins_on_screen -= 1
                 if CoinXposs10 != 0 and CoinYposs10 != 0 and Coin10 in objects:
+                    CoinXposs10 = 0
+                    CoinYposs10 = 0
                     objects.remove(Coin10)
+                    coins_on_screen -= 1
             player.x = WINDOW_SIZE[0] / 2 - 25
             player.y = WINDOW_SIZE[1]
             enemy_spawner1()
