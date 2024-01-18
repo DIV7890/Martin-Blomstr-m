d9f029a776038f2ea250e91fe484472e0753c40f
Martin Blomström <marblo070718@student.jenseneducation.se>
2024-01-18 14:29:33 +0100

    neigour2

--- Error!.py
+++ Error!.py
@@ -4,21 +4,32 @@ import pygame as pg
 import random
 import math as m
 import pyautogui
+import keyboard as k
 
 # Alla variabler samlas här
 Frame_Rate = 60
 Run = True
+Window_Center = (0 , 0)
 
 # initierar pygame skärmen och dess tickrate
 Clock = pg.time.Clock()
 screen_width, screen_height = pyautogui.size()
 WINDOW_SIZE = [screen_width, screen_height]
 screen = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption('Error!')
 
 
 def update_screen():
     Clock.tick(Frame_Rate)
     pg.display.update()
+
+
+def check_for_input(key):
+    if key == pg.K_ESCAPE:
+        exit()
+
+
+
 class Object:
 
     def __init__(self, x, y, width, height, image):
@@ -28,11 +39,14 @@ class Object:
         self.height = height
         self.image = image
 
-Background = Object(0, 0, screen_width, screen_height, pg.image.load("bg.jpg"))
-
+background = pg.transform.scale(pg.image.load("bg.jpg"), WINDOW_SIZE)
+screen.blit(background, Window_Center)
 while Run:
     update_screen()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        elif event.type == pg.KEYDOWN:
+            check_for_input(event.key)
+
 
---
+++ damp.py
@@ -0,0 +1,18 @@
+import pyautogui as pag
+import keyboard
+import time
+Run = False
+time.sleep(3)
+
+while True:
+    while Run:
+        if keyboard.is_pressed("0"):
+            Run = False
+
+        pag.click(delay=)
+
+    if keyboard.is_pressed("0"):
+        Run = True
+
+
+
