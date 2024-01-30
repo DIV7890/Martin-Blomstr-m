15237f4406dcbe567218aebe5bcd04707d4ea3a8
DIV7890 <marblo070718@student.jenseneducation.se>
2024-01-30 19:11:56 +0100

    Error!

--- Error!.py
+++ Error!.py
@@ -11,6 +11,8 @@ Frame_Rate = 60
 Run = True
 Objects = []
 Buttons = []
+Entitys = []
+Black = (0,0,0)
 
 
 # initierar pygame skärmen och dess tickrate
@@ -34,6 +36,20 @@ def check_for_input(key):
 
 def home_screen():
     Start_Button = Button(Window_Center[0], Window_Center[1], 200, 120, pg.image.load("start_button.png").convert_alpha(), "start")
+    while Run:
+        update_screen()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+            elif event.type == pg.KEYDOWN:
+                check_for_input(event.key)
+
+        for b in Buttons:
+            b.is_clicked()
+
+def playing():
+    screen.fill(Black)
+    entity1 = Entity(Window_Center[0], Window_Center[1], 200, 120, pg.image.load("start_button.png").convert_alpha(), 1)
     while Run:
         update_screen()
         for event in pg.event.get():
@@ -74,12 +90,44 @@ class Button(Object): # knapp klassen är ett object i spelet men tar även argu
 
     def is_clicked(self):
         if int(pg.mouse.get_pos()[0]) in self.area[0] and int(pg.mouse.get_pos()[1]) in self.area[1] and pg.mouse.get_pressed(num_buttons=3)[0]:
+            if self.type == "start":
+                playing()
             self.remove()
             Object.remove(self)
 
     def remove(self):
         Buttons.remove(self)
-        print("removed button")
+
+class Entity(Object):
+    def __init__(self, x, y, width, height, image, health):
+        super().__init__(x, y, width, height, image)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.health = health
+        self.position = (self.x - self.width / 2, self.y - self.height / 2)
+        self.area = (range(int(self.position[0] - self.width), int(self.position[0] + self.width)), range(int(self.position[1] - self.height), int(self.position[1] + self.height)))
+
+        Entitys.append(self)
+
+        if self.health <= 0:
+            self.remove()
+
+    def is_clicked(self):
+        if int(pg.mouse.get_pos()[0]) in self.area[0] and int(pg.mouse.get_pos()[1]) in self.area[1] and pg.mouse.get_pressed(num_buttons=3)[0]:
+            self.health -= 1
+
+    def remove(self):
+        Entitys.remove(self)
+        Objects.remove(self)
+        self.x = -1
+        self.y = -1
+        self.width = -1
+        self.height = -1
+        self.image = "transparent.png"
+
 
 
 
